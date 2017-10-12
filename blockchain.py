8d86f9e40fd603349be5aa6a5f75d040b00633d3
jackjdl <jjdl@st-andrews.ac.uk>
2017-10-12 14:16:38 +0100

    Implemented Step 4

--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,8 @@ import hashlib
 import json
 from textwrap import dedent
 from time import time
+from urllib.parse import urlparse
+import requests
 
 
 class Blockchain(object):
@@ -11,6 +13,24 @@ class Blockchain(object):
 
         # Create the genesis block
         self.new_block(proof=100, previous_hash=1)
+
+        # Create an empty set of nodes, ensures no duplicates
+        self.nodes = set()
+
+
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes
+
+        address <str> -- Address of the node (e.g 'http://192.168.0.5:5000')
+
+        """
+
+        # Parse the adress
+        parsed_url = urlparse(address)
+
+        # Add the network location of the address to the list of nodes
+        self.nodes.add(parsed_url.netloc)
 
 
     def new_block(self, proof, previous_hash=None):
@@ -97,9 +117,9 @@ class Blockchain(object):
          - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
          - p is the previous proof, and p' is the new proof
 
-        last_proof <int> -- the previous Block's proof
+        last_proof <int> -- The previous Block's proof
 
-        return <int> -- the new Block's proof
+        return <int> -- The new Block's proof
 
         """
 
@@ -118,8 +138,8 @@ class Blockchain(object):
         """
         Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
 
-        last_proof <int> -- the previous Block's proof
-        proof <int> -- the new Block's proof
+        last_proof <int> -- The previous Block's proof
+        proof <int> -- The new Block's proof
 
         return <bool> True if correct, False if not
 
@@ -132,3 +152,65 @@ class Blockchain(object):
         guess_hash = hashlib.sha256(guess).hexdigest()
 
         return guess_hash[:4] == "0000"
+
+
+    def valid_chain(self, chain):
+        """
+        Determine if a given blockchain is valid
+
+        chain <list> -- the given blockchain
+
+        return <bool> -- whether the given blockchain is valid or not
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n----------\n")
+
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+
+    def resolve_conflicts(self):
+        """
+        This is our Consensus Algorithm, it resolves conflicts
+        by replacing our chain with the longest one in the network.
+
+        return <bool> -- True if our chain was replaces, False if not
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+
+        # We're only looking for chains longer than ours
+        max_length = len(self.chain)
+
+        # Grab and verify the chains from all the nodes in our network
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Check if the length if longer and the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Replace our chain if we discovered a new, valid chain longer than ours
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
